cdd4df23a15e939b646aab58e38cb5f13bf7fad7
LFourOne <143569965+LFourOne@users.noreply.github.com>
2024-06-03 18:32:33 -0400

    Add files via upload

---
+++ graphs.js
@@ -0,0 +1,75 @@
+
+const chart1 = document.getElementById('chartOne');
+
+new Chart(chart1, {
+type: 'bar',
+data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+    label: 'Example 1',
+    data: [12, 19, 3, 5, 2, 3],
+    borderWidth: 1,
+    backgroundColor: [
+        '#c1d84e'
+    ]
+    }]
+},
+options: {
+    indexAxis: 'y',
+    scales: {
+    x: {
+        beginAtZero: true
+    }
+    }
+}
+});
+
+const chart2 = document.getElementById('chartTwo');
+
+new Chart(chart2, {
+type: 'bar',
+data: {
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [{
+    label: 'Example 2',
+    data: [15, 7, 10],
+    borderWidth: 1,
+    backgroundColor: [
+        '#c1d84e'
+    ]
+    }]
+},
+options: {
+    indexAxis: 'y',
+    scales: {
+    x: {
+        beginAtZero: true
+    }
+    }
+}
+});
+
+const chart3 = document.getElementById('chartThree');
+
+new Chart(chart3, {
+type: 'bar',
+data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+    label: 'Example 3',
+    data: [16, 12, 1, 30, 5, 5],
+    borderWidth: 1,
+    backgroundColor: [
+        '#c1d84e'
+    ]
+    }]
+},
+options: {
+    indexAxis: 'y',
+    scales: {
+    x: {
+        beginAtZero: true
+    }
+    }
+}
+});
